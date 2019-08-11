60760cf655424607d9253b7de78681b6f70ebfc3
pratiklodha <pratiklodha01@gmail.com>
2019-08-11 11:12:56 +0530

    Updates

---
+++ precache-manifest.aa7ca96a2dc4a1fe4f7baf3da4d6f9be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661d04fab4f172ba292e635202c97178",
+    "url": "/pokemon-cards/index.html"
+  },
+  {
+    "revision": "e393e3bf0cf4ccf0d521",
+    "url": "/pokemon-cards/static/css/main.c17da288.chunk.css"
+  },
+  {
+    "revision": "eeb7bdf49274f5df9522",
+    "url": "/pokemon-cards/static/js/2.034f072a.chunk.js"
+  },
+  {
+    "revision": "e393e3bf0cf4ccf0d521",
+    "url": "/pokemon-cards/static/js/main.7abce622.chunk.js"
+  },
+  {
+    "revision": "13a066824fc6b1e83008",
+    "url": "/pokemon-cards/static/js/runtime~main.19a1eb3f.js"
+  }
+]);
